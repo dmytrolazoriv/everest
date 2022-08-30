e34ad1b194f42a61718e61d7ee312df188a13b9f
dmytrolazoriv <38793231+dmytrolazoriv@users.noreply.github.com>
2022-08-30 18:29:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,4 +20,44 @@ $(function () {
             $(".main-nav__burger:last-child").css("display", "block");
         }
     });
+
+    $(".main-nav__button").on("click", function (event) {
+        event.stopPropagation();
+        $(this).toggleClass("active");
+        $(".catalog__modal").toggleClass("activeModal");
+        $(".overlay").toggleClass("active");
+    });
+
+    $(".main-nav__button").on("keyup", function (event) {
+        event.stopPropagation();
+        if (event.key === "Escape") {
+            $(".catalog__modal").removeClass("activeModal");
+            $(".main-nav__button").removeClass("active");
+            $(".overlay").removeClass("active");
+        }
+    });
+
+    $(document).on("click", function (event) {
+        if ($(event.target).closest($(".catalog__modal")).length != 0) return;
+        // if ($(event.target).closest($(".catalog__modal")).length != 0) return false;
+        $(".catalog__modal").removeClass("activeModal");
+        $(".main-nav__button").removeClass("active");
+        $(".overlay").removeClass("active");
+        // Check if the filter list parent element exist
+        // var isClosest = event.target.closest(modalSelector);
+
+        // If `isClosest` equals falsy & popup has the class `show`
+        // then hide the popup
+        // if (!isClosest && modalEl.hasClass("active")) {
+        //     $(".catalog__modal").removeClass("active");
+        //     $(".main-nav__button").removeClass("active");
+        //     $(".overlay").removeClass("active");
+        // }
+    });
+
+    $(".menu__btnclose").on("click", function () {
+        $(".catalog__modal").removeClass("activeModal");
+        $(".main-nav__button").removeClass("active");
+        $(".overlay").removeClass("active");
+    });
 });
